e7e68b9b1bf17123d2411b9b19f4320daba87132
FIZZI-77 <zaprudnovfedor@gmail.com>
2024-11-28 18:41:07 +0300

    изменение тестов

--- hometask_3/config_translator/tests.py
+++ hometask_3/config_translator/tests.py
@@ -1,17 +1,26 @@
 import unittest
-from config_translator.constants import evaluate_constant_expression
+from config_translator.constants import evaluate_constant_expression  # Импортируем функцию из constants
 
 class TestParser(unittest.TestCase):
-    def test_evaluate_constant_expression(self):
-        # Тесты с числами
+    
+    # Тесты с числами
+    def test_addition(self):
         self.assertEqual(evaluate_constant_expression(".{ 3 4 + }."), 7)
+
+    def test_power(self):
         self.assertEqual(evaluate_constant_expression(".{ 2 3 pow() }."), 8)  # Пример с степенью
+
+    def test_subtraction(self):
         self.assertEqual(evaluate_constant_expression(".{ 3 4 - }."), -1)
 
+    # Тесты с массивами
+    def test_len_string_array(self):
+        self.assertEqual(evaluate_constant_expression(".{ len([ 'Fiction', 'Science', 'Biography' ]) }."), 3)  # Длина массива строк
 
-        # Тесты с массивами
-        self.assertEqual(evaluate_constant_expression(".{ len([ 'Fiction', 'Science', 'Biography' ]) }."), 3)  # Длина массива
+    def test_len_numeric_array(self):
         self.assertEqual(evaluate_constant_expression(".{ len([ 1, 2, 3, 4 ]) }."), 4)  # Длина массива чисел
+
+    def test_len_mixed_array(self):
         self.assertEqual(evaluate_constant_expression(".{ len([ 'apple', 'banana', 'cherry' ]) }."), 3)  # Длина массива строк
 
 if __name__ == '__main__':
